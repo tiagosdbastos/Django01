606f454db38e5666a2ae25ed3fd01fd59d578493
tiagosdbastos <tiagosdbastos@gmail.com>
2026-05-16 21:46:34 -0300

    Update Python version, add initial migration, enhance Fotografia model, and update index view and template for dynamic data rendering

--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def index(request):
-    return render(request, "galeria/index.html")
+
+    dados = {
+    1: {
+        "nome": "Nebulosa de Carina",
+        "legenda": "webtelescope.org / NASA / James Webb"},
+    2: {
+        "nome": "Galáxia NGC 1079",
+        "legenda": "nasa.org / NASA / Hubble" },
+        }
+
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
-    return render(request, "galeria/imagem.html")
+    return render(request, 'galeria/imagem.html')
